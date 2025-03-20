fd0202825439fabafa4da38d56c301a8cb60348c
SimpNiLouise <kengopez26@gmail.com>
2025-03-20 19:44:40 +0800

    userSettings

---
+++ src/customerPages/userSettingsPage.php
@@ -0,0 +1,39 @@
+<section class="usettings">
+    <div class="usidebarwrapper">
+        <!-- User Info Box -->
+        <div class="uboxwrapper">
+            <div class="ubox1">
+                <img src="../images/logo.png" alt="User Logo">
+                <div class="ubox1info">
+                    <h1>Fname Lname</h1>
+                    <p>customer@example.com</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navigation Buttons -->
+        <div class="nav-buttons mt-4">
+            <a href="?page=userSettingsPage" class="nav-button fs-5 usersettingsactive" id="profileLink">
+                <i class="fa-solid fa-user fa-fw"></i> Profile
+            </a>
+            <a href="?page=userSettings" class="nav-button fs-5" id="settingsLink">
+                <i class="fa-solid fa-gear fa-fw"></i> Settings
+            </a>
+            <a href="?page=userOrders" class="nav-button fs-5" id="ordersLink">
+                <i class="fa-solid fa-receipt fa-fw"></i> Orders
+            </a>
+        </div>
+
+        <!-- Logout Button -->
+        <div class="logout-button mt-4">
+            <a href="Login.php" class="btn btn-danger w-100 fs-5">
+                <i class="fa-solid fa-right-from-bracket"></i> Logout
+            </a>
+        </div>
+    </div>
+
+    <!-- Content Area -->
+    <div class="ucontent">
+
+    </div>
+</section>
